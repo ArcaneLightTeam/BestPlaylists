661e2fd9021185d2f1335c89b61fe0d35b0168d0
M-Yankov <pointblank12345@abv.bg>
2016-01-23 23:58:09 +0200

    Home page Added

---
+++ Source/BestPlaylists.WebForms/Home.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="BestPlaylists.WebForms.Home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+
+        <h2 class="text-center text-success">Top Playlists</h2>
+        <asp:GridView AutoGenerateColumns="false" CssClass="table text-center table-borderer" ItemType="BestPlaylists.Data.Models.Playlist" ID="gridTopPLaylists" runat="server">
+            <Columns>
+                <asp:BoundField DataField="Title" HeaderText="Title" />
+                <asp:BoundField DataField="Description" HeaderText="Description" />
+                <asp:BoundField DataField="Date" DataFormatString="{0:dd-MM-yyyy}" HeaderText="Date" />
+                <asp:BoundField DataField="Rating" DataFormatString="{0:N2}" HeaderText="Rating" />
+                <asp:BoundField DataField="Category" HeaderText="Category" />
+                <asp:BoundField DataField="Username" HeaderText="User" />
+            </Columns>
+            <HeaderStyle CssClass="bg-success" />
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ Source/BestPlaylists.WebForms/Home.aspx.cs
@@ -0,0 +1,52 @@
+﻿namespace BestPlaylists.WebForms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.UI;
+    using System.Web.UI.WebControls;
+    using BestPlaylists.Services.Data.Contracts;
+    using Models;
+    using Ninject;
+
+    public partial class Home : Page
+    {
+        [Inject]
+        public IPlaylistService Playlists { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // CONTSANTS - when common class library exist
+            // expires, playlists, 10 for playlist count and 
+            object expires = this.Cache["expires"];
+
+            if (expires != null && DateTime.Parse(expires.ToString()) > DateTime.Now)
+            {
+                this.gridTopPLaylists.DataSource = this.Cache["playlists"] as IList<PlaylistResponseModel>;
+                this.gridTopPLaylists.DataBind();
+                return;
+            }
+
+            IList<PlaylistResponseModel> topPlaylists =
+                Playlists.GetAll()
+                .OrderByDescending(p => p.Ratings.Average(c => c.Value))
+                .Take(10)
+                .Select(p => new PlaylistResponseModel
+                {
+                    Title = p.Title,
+                    Description = p.Description,
+                    Rating = p.Ratings.Average(c => c.Value),
+                    Category = p.Category.Name,
+                    Username = p.User.UserName,
+                    Date = p.CreationDate
+                })
+                .ToList();
+
+            this.Cache["playlists"] = topPlaylists;
+            this.Cache["expires"] = DateTime.Now.AddMinutes(10);
+            
+            this.gridTopPLaylists.DataSource = topPlaylists;
+            this.gridTopPLaylists.DataBind();
+        }
+    }
+}
