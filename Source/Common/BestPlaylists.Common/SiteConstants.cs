9e58dddb9a707dbe88a479a08d2aad0e7b8e124a
M-Yankov <pointblank12345@abv.bg>
2016-01-25 10:08:10 +0200

    Introduced constants for Default page

--- Source/Common/BestPlaylists.Common/SiteConstants.cs
+++ Source/Common/BestPlaylists.Common/SiteConstants.cs
@@ -2,5 +2,9 @@
 {
     public class SiteConstants
     {
+        public const int HomePlaylistsSize = 10;
+        public const int MinutesToKeepCache = 10;
+        public const string CacheExpiresKey = "expires";
+        public const string CachePlaylistsKey = "playlists";
     }
 }
