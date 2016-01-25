3e6442be58419f453b679678cb5b98ce2cf58ffb
Deyan Todorov <deyan.todorov@hotmail.com>
2016-01-26 00:14:51 +0200

    Add nice seed data. I hope it works well...

--- Source/Data/BestPlaylists..Data/Migrations/Configuration.cs
+++ Source/Data/BestPlaylists..Data/Migrations/Configuration.cs
@@ -26,6 +26,7 @@ namespace BestPlaylists.Data.Migrations
             this.SeedRoles(context);
             this.SeedUsers(context);
             this.SeedPlaylists(context);
+            this.SeedComments(context);
         }
 
         private void SeedCategories(BestPlaylistsDbContext context)
@@ -107,12 +108,36 @@ namespace BestPlaylists.Data.Migrations
                 manager.Create(user, DbConstants.Editor1Pass);
                 manager.AddToRole(user.Id, DbConstants.RoleEditor);
             }
+            
+            // Adding 20 more users
+            for (int i = 1; i <= 20; i++)
+            {
+                var userName = DbConstants.Editor1Name + i;
+
+                if (!context.Users.Any(u => u.UserName == userName))
+                {
+                    var user = new User()
+                    {
+                        UserName = userName,
+                        Email = userName + "@gmail.com",
+                        FirstName = userName,
+                        LastName = userName
+                    };
+
+                    manager.Create(user, DbConstants.Editor1Pass);
+                    manager.AddToRole(user.Id, DbConstants.RoleEditor);
+                }
+            }
         }
 
         private void SeedPlaylists(BestPlaylistsDbContext context)
         {
             var categoriesCount = context.Categories.Count();
-            var admin1Id = context.Users.Single(u => u.UserName == DbConstants.Admin1Name).Id;
+
+            var allUsers = context
+                .Users
+                .Select(x => x.Id)
+                .ToList();
 
             if (!context.Playlists.Any())
             {
@@ -120,17 +145,15 @@ namespace BestPlaylists.Data.Migrations
 
                 for (int i = 0; i < 100; i++)
                 {
-                    var videos = new List<Video>();
-
                     var playlist = new Playlist()
                     {
-                        Title = randomGenerator.GetRandomString(ModelsConstats.MinVideoTitleLength, ModelsConstats.MaxVideoTitleLength),
-                        Description = randomGenerator.GetRandomString(ModelsConstats.MinVideoTitleLength, ModelsConstats.MaxVideoDescriptionLength - 1500),
+                        Title = randomGenerator.GetRandowSentance(ModelsConstats.MinVideoTitleLength, ModelsConstats.MaxVideoTitleLength - 50),
+                        Description = randomGenerator.GetRandowSentance(ModelsConstats.MinVideoTitleLength, ModelsConstats.MaxVideoDescriptionLength - 1800),
                         CreationDate = randomGenerator.GetRandomDate(DateTime.Now.AddYears(-i), DateTime.Now),
                         IsPrivate = (i % 2 == 0),
                         CurrentRating = randomGenerator.GetRandomNumber(1, 5) % 6,
                         CategoryId = randomGenerator.GetRandomNumber(1, categoriesCount),
-                        UserId = admin1Id,
+                        UserId = allUsers[randomGenerator.GetRandomNumber(0, allUsers.Count - 1)],
                     };
 
                     context.Playlists.Add(playlist);
@@ -149,8 +172,8 @@ namespace BestPlaylists.Data.Migrations
                         var video = new Video()
                         {
                             PlaylistId = i + 1,
-                            Url = "http://www.youtube.com/",
-                            UserId = admin1Id
+                            Url = GetRandomVideo(),
+                            UserId = context.Playlists.First(id => id.Id == i + 1).UserId
                         };
 
                         context.Videos.Add(video);
@@ -159,6 +182,97 @@ namespace BestPlaylists.Data.Migrations
                     context.SaveChanges();
                 }
             }
+        }
+
+        private void SeedComments(BestPlaylistsDbContext context)
+        {
+            if (!context.Comments.Any())
+            {
+                var randomGenerator = new RandomGenerator();
+                var playLists = context.Playlists.ToList();
+                var users = context.Users.Select(x => x.Id).ToList();
+
+                foreach (var playlist in playLists)
+                {
+                    var commentsCount = randomGenerator.GetRandomNumber(3, 9);
+
+                    for (int i = 0; i < commentsCount; i++)
+                    {
+                        playlist.Comments.Add(new Comment()
+                        {
+                            CreationDate = randomGenerator.GetRandomDate(DateTime.Now.AddYears(-i), DateTime.Now),
+                            Text = randomGenerator.GetRandowSentance(ModelsConstats.MinVideoTitleLength, ModelsConstats.MaxVideoDescriptionLength - 1800),
+                            UserId = users[randomGenerator.GetRandomNumber(0, users.Count - 1)]
+                        });
+                        
+                        context.Playlists.AddOrUpdate(playlist);
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        private string GetRandomVideo()
+        {
+            var youTubeVideos = new List<string>()
+            {
+                "https://www.youtube.com/watch?v=bVRfloSVFFs",
+                "https://www.youtube.com/watch?v=EIxfpanpyrs",
+                "https://www.youtube.com/watch?v=KxlcEOY-7hg",
+                "https://www.youtube.com/watch?v=1RjE915E4mg",
+                "https://www.youtube.com/watch?v=OGWehEcKArE",
+                "https://www.youtube.com/watch?v=BiADfKN_1Sc",
+                "https://www.youtube.com/watch?v=62OL7SC5_A8",
+                "https://www.youtube.com/watch?v=O6gkm-nklyw",
+                "https://www.youtube.com/watch?v=lPmOcJ9YdYw",
+                "https://www.youtube.com/watch?v=b6m-XlOxjbk",
+                "https://www.youtube.com/watch?v=tPxcFm_S1qc",
+                "https://www.youtube.com/watch?v=LTIJZJsLG8o",
+                "https://www.youtube.com/watch?v=q2DqctUI8ho",
+                "https://www.youtube.com/watch?v=Nqh3LI6l1fk",
+                "https://www.youtube.com/watch?v=oq2mykyhl-0",
+                "https://www.youtube.com/watch?v=6H9aHntpjcE",
+                "https://www.youtube.com/watch?v=gojyDYI8bzY",
+                "https://www.youtube.com/watch?v=nWLYUdejPWc",
+                "https://www.youtube.com/watch?v=4Esni96oBdQ",
+                "https://www.youtube.com/watch?v=1CxPGDZJPDw",
+                "https://www.youtube.com/watch?v=fk9iJougDFE",
+                "https://www.youtube.com/watch?v=GF60Iuh643I",
+                "https://www.youtube.com/watch?v=59Zcx9YbZxI",
+                "https://www.youtube.com/watch?v=VAZ5te90vlc",
+                "https://www.youtube.com/watch?v=APoCpZOrI4w",
+                "https://www.youtube.com/watch?v=APoCpZOrI4w",
+                "https://www.youtube.com/watch?v=V4LnorVVxfw",
+                "https://www.youtube.com/watch?v=Cs4OUFAr5Hc",
+                "https://www.youtube.com/watch?v=MBsEPgjE7KU",
+                "https://www.youtube.com/watch?v=axm-N9L2c_E",
+                "https://www.youtube.com/watch?v=dGpZEuXoFWw",
+                "https://www.youtube.com/watch?v=JgzgcAlpxNU",
+                "https://www.youtube.com/watch?v=0FEYvKxCnYw",
+                "https://www.youtube.com/watch?v=MveqXxB12YA",
+                "https://www.youtube.com/watch?v=-4FlKZIo8B4",
+                "https://www.youtube.com/watch?v=JWZMzcmqMwc",
+                "https://www.youtube.com/watch?v=Snw0rEWyvLk",
+                "https://www.youtube.com/watch?v=DaxB9y--CGI",
+                "https://www.youtube.com/watch?v=Md7bfNTR0TM",
+                "https://www.youtube.com/watch?v=zmIxovkV3Ro",
+                "https://www.youtube.com/watch?v=tntOCGkgt98",
+                "https://www.youtube.com/watch?v=CE-JlvmnRtY",
+                "https://www.youtube.com/watch?v=UIrEM_9qvZU",
+                "https://www.youtube.com/watch?v=25OUFtdno8U",
+                "https://www.youtube.com/watch?v=HxM46vRJMZs",
+                "https://www.youtube.com/watch?v=G8KpPw303PY",
+                "https://www.youtube.com/watch?v=mW3S0u8bj58",
+                "https://www.youtube.com/watch?v=-hIugp7p5O0",
+                "https://www.youtube.com/watch?v=9Mk_t78IeO8",
+                "https://www.youtube.com/watch?v=tcPVzzyB7OI",
+                "https://www.youtube.com/watch?v=JgjXG_9L_Jw",
+            };
+
+            var randomGenerator = new RandomGenerator();
+            var videoUrl = youTubeVideos[randomGenerator.GetRandomNumber(0, youTubeVideos.Count - 1)];
+
+            return videoUrl;
         }
     }
 }
